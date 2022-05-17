e9e23c03ba7cae7cbd17307b9243e6619281c34a
tranngocdam <tranngocdam12gmail.com>
2022-05-18 06:52:41 +0700

    nop bai tap ngay 18/5

--- module3/ss2_thiet_ke_tao_csdl/bai_tap/quan_li_ban_hang.sql
+++ module3/ss2_thiet_ke_tao_csdl/bai_tap/quan_li_ban_hang.sql
@@ -2,14 +2,12 @@ drop database if exists quan_ly_ban_hang;
 create database quan_ly_ban_hang;
 use quan_ly_ban_hang;
 
--- drop table if exists customer;
 create table customer(
 cID int,
 cName varchar(50),
 cAge int,
 primary key(cID));
 
--- drop table if exists `order`;
 create table `order`(
 oID int,
 cID int,
@@ -18,14 +16,12 @@ oTotalPrime double,
 primary key(oID),
 foreign key (cID) references customer(cID));
 
--- drop table if exists product;
 create table product(
 pID int,
 pName varchar(50),
 pPrime double,
 primary key(pID));
 
--- drop table if exists orderDetail;
 create table orderDetail(
 oID int,
 pID int,
@@ -34,3 +30,37 @@ primary key(oID, pID),
 foreign key (oID) references `order`(oID),
 foreign key (pID) references product(pID));
 
+insert into customer
+value(1, 'Minh Quan', 10), (2, 'Ngoc Anh', 20), (3, 'Hong Ha', 50);
+
+insert into `order`
+value(1, 1 , '2006-03-21', null), (2, 2, '2006-03-23', null), (3, 1, '2006-03-16', null);
+
+insert into product
+value(1, 'May Giac', 3), (2, 'Tu Lanh', 5), (3, 'Dieu Hoa', 7), (4, 'Quat', 1), (5, 'Bep Dien', 2);
+
+insert into orderDetail
+value(1, 1, 3), (1, 3, 7), (1, 4, 2), (2, 1, 1), (3, 1, 8), (2, 5, 4), (2, 3, 3);
+
+select `order`.oID, `order`.oDate, product.pPrime from `order`
+join  orderDetail on `order`.oID = orderDetail.oID
+join product on product.pID = orderDetail.pID;
+
+select customer.cID, customer.cName, product.pName from customer
+join `order` on customer.cID = `order`.cID
+join orderDetail on `order`.oID = orderDetail.oID
+join product on product.pID = orderDetail.pID;
+
+select cName from customer
+where cName not in (
+	select customer.cName from customer 
+	join `order` on customer.cID = `order`.cID
+	join orderDetail on `order`.oID = orderDetail.oID
+	join product on product.pID = orderDetail.pID
+	group by customer.cName
+);
+
+select `order`.oID, `order`.oDate, sum(orderDetail.odQTy*product.pPrime) as oTotalPrime from `order`
+join orderDetail on `order`.oID = orderDetail.oID
+join product on product.pID = orderDetail.pID
+group by `order`.oID
--- module3/ss2_thiet_ke_tao_csdl/thuc_hanh/quan_li_sinh_vien.sql
+++ module3/ss2_thiet_ke_tao_csdl/thuc_hanh/quan_li_sinh_vien.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE quan_ly_sinh_vien;
 USE quan_ly_sinh_vien;
+-- drop database quan_ly_sinh_vien;
 CREATE TABLE class(
     class_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     class_name VARCHAR(60) NOT NULL,
     startDate DATETIME    NOT NULL,
     status    BIT
 );
+-- drop table student;
 CREATE TABLE student(
     student_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_name VARCHAR(30) NOT NULL,
@@ -22,6 +24,7 @@ CREATE TABLE subject(
     credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
     status  BIT                  DEFAULT 1
 );
+
 CREATE TABLE mark(
     mark_id    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     sub_id     INT NOT NULL,
@@ -32,3 +35,37 @@ CREATE TABLE mark(
     FOREIGN KEY (sub_id) REFERENCES subject(sub_id),
     FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
+
+insert into class
+value (1, 'A1', '2008-12-08', 1), (2, 'A2', '2008-12-22', 1),(3, 'B3', current_date(), 0);
+
+insert into student(student_name, student_address, student_phone, status, class_id)
+value('Hung', 'Ha Noi', '0912113113', 1, 1), ('Manh', 'HCM', '0123123123', 0, 2);
+insert into student(student_name, student_address, status, class_id)
+value ('Hoa', 'Hai phong', 1, 1);
+
+insert into subject
+value(1, 'CF', 5, 1), (2, 'C', 6, 1), (3, 'HDJ', 5, 1), (4, 'RDBMS', 10, 1);
+
+insert into mark(sub_id, student_id, mark, ExamTimes)
+value(1, 1, 8, 1), (1, 2, 10, 2), (2, 1, 12, 1);
+
+select * from student
+where (student_name like 'h%');
+
+select * from class
+where (month(startDate)='12');
+
+select subject.sub_name from subject
+where credit between 3 and 5;
+
+set sql_safe_updates=0;
+update student
+set class_id='2'
+where (student.student_name='Hung');
+set sql_safe_updates=1;
+
+select student.student_name, subject.sub_name, mark.mark from student
+join mark on student.student_id = mark.student_id
+join subject on subject.sub_id = mark.sub_id
+order by mark.mark desc, subject.sub_name
